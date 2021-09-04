5d344ad285cd7894b7e2e5c15f345970e5e17b71
Leo Ding Hao <leo.ding.hao.2014@gmail.com>
2021-09-04 22:44:26 +0800

    fix: add try catch block for token refresh
    Token refresh method causes some error at the moment. Seems like
    a python library issue. Don't really want to debug the method ath the
    moment so the easier way is to delete the token and request for it
    again.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from typing import Tuple
 
 from tqdm import tqdm
 from google.auth.transport.requests import Request
+from google.auth.exceptions import RefreshError
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
@@ -52,7 +53,13 @@ class Drive:
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
+                # cause creds.refresh doesn't work for some reason.
+                # faster to just delete the token and request another
+                try:
+                    creds.refresh(Request())
+                except RefreshError:
+                    os.remove(token_path)
+                    return self.__get_creds()
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
                     credentials_path, Drive.SCOPES
